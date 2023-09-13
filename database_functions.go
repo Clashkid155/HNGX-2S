6fbdda87c20c190c2de8b192deed685fa4814ece
ClashKid155 <beluchukwunjike@gmail.com>
2023-09-13 15:45:13 +0100

    Finish up other db methods
    Signed-off-by: ClashKid155 <beluchukwunjike@gmail.com>

--- database_functions.go
+++ database_functions.go
@@ -12,7 +12,7 @@ import (
 	"time"
 )
 
-func createUser(user string) (id string, err error) {
+func createUser(user string) (id int, err error) {
 	exec := dbCon.QueryRow(context.Background(), "INSERT INTO persons(name) VALUES($1) RETURNING id", user)
 
 	err = exec.Scan(&id)
@@ -24,13 +24,64 @@ func createUser(user string) (id string, err error) {
 	}
 
 	fmt.Println("ID ", id)
-	//fmt.Printf("%v\nType: %T", exec, id)
+
 	return
 }
-func updateUser() {
+func updateUser(userId string, newName string) (id int, err error) {
 
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	if isDigit(userId) {
+		id, err = strconv.Atoi(userId)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		_, err = dbCon.Exec(ctx, ""+
+			"UPDATE persons SET name = $1 WHERE id = $2", newName, id)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	} else {
+		p := dbCon.QueryRow(ctx, ""+
+			"UPDATE persons SET name = $1 WHERE  id = $2 RETURNING id", newName, userId)
+		err = p.Scan(&id)
+		if err != nil {
+			log.Println("Error from, GetUser: ", err)
+		}
+	}
+
+	return
 }
-func deleteUser() {
+func deleteUser(value string) (err error) {
+
+	var id int
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	if isDigit(value) {
+		id, err = strconv.Atoi(value)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		_, err = dbCon.Exec(ctx, ""+
+			"DELETE FROM persons WHERE id = $1", id)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	} else {
+		_, err = dbCon.Exec(ctx, ""+
+			"DELETE FROM persons WHERE name = $1", value)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+	return
 
 }
 func getUser(value string) (name string, idDb int, err error) {
